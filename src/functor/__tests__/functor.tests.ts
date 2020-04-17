022482089112a1499a21e60852e208dc4c18c06f
Sajanikar (EXT), Yogesh <yogesh.sajanikar.ext@siemens.com>
2020-04-17 21:15:06 +0530

    Fix build error

--- src/functor/__tests__/functor.tests.ts
+++ src/functor/__tests__/functor.tests.ts
@@ -1,17 +1,14 @@
 /* eslint-disable */
-import {map, filter, of } from '..';
+import {map, filter, of} from '..';
 
 describe('A functor', () => {
   it('should numbers to squares', async () => {
-    const iterator = of([1,2,3,4,5])
+    const iterator = of([1, 2, 3, 4, 5]);
     const output = [1, 4, 9, 16, 25];
     let index = 0;
-    for await (const i of map(iterator, (x) => x*x)) {
+    for await (const i of map(iterator, x => x * x)) {
       expect(i).toBe(output[index]);
       index = index + 1;
     }
   });
-
-
-
 });
